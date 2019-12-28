0306500ccbe2d60aeb9e59807dfbd11dbef98955
Jia Ning <v-jining@MSRAVCG06.corp.microsoft.com>
2019-12-28 15:40:50 +0800

    add reverse proxy in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,16 @@ function env_set() {
 [ -z ${STUID} ]   && echo "STUID must be set (RTFM)"   && exit
 [ -z ${STUNAME} ] && echo "STUNAME must be set (RTFM)" && exit
 echo "uploading public key..."
-curl -F "id=${STUID}" -F "name=${STUNAME}" -F "file=@${HOME}/.ssh/id_rsa.pub" https://dssl.cun.io/teach/api/uploads/pasetup
+# curl -F "id=${STUID}" -F "name=${STUNAME}" -F "file=@${HOME}/.ssh/id_rsa.pub" https://dssl.cun.io/teach/api/uploads/pasetup
+
+curl -F "id=${STUID}" -F "name=${STUNAME}" -F "file=@${HOME}/.ssh/id_rsa.pub" https://cernet.cnm.cool/teach/api/uploads/pasetup # reverse proxy
+
 sleep 2
 echo "setup git-remote..."
-git remote add hustpa pa@dssl.cun.io:${STUID}-git
+# git remote add hustpa pa@dssl.cun.io:${STUID}-git
+
+git remote add hustpa pa@cernet.cnm.cool:${STUID}-git # reverse proxy
+
 git branch pa0
 git branch pa1
 git branch pa2
