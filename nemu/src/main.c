7fd0a6e23923d3a4f2be5ccf970faa5d78e19aa5
tracer-ics2018 <tracer@njuics.org>
2019-12-31 00:03:38 +0800

    >  compile U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  00:03:38 up 39 days,  9:31,  3 users,  load average: 0.03, 0.10, 0.08 96b46a211cf5a365cec4f96ede9ff39ee6ba5cf1

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
   int cnt = 0;
   while (fscanf(fp, "%u %[^\n]", &res, str) == 2)
   {
-    printf("%d\n", res);
+    printf("%u\t%s\n", res, str);
     unsigned tmp = expr(str, &success);
     printf("%d\n", tmp);
     pass = pass && success && (tmp == res);
