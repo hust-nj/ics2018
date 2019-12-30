5b6b26ac3715f7c12fa47e1104eaad7351dc8dca
Jia Ning <v-jining@MSRAVCG06.corp.microsoft.com>
2019-12-30 22:47:55 +0800

    generate test input file

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -7,8 +7,65 @@
 
 // this should be enough
 static char buf[65536];
+static char *p;
+// generate number in [lower, upper]
+int gen_rand_num(unsigned lower, unsigned upper)
+{
+  return (rand() % (upper-lower+1)) + lower;
+}
+
+
+void dfs(int dep)
+{
+  if (dep == 0)
+  {
+    int num = gen_rand_num(0, 30);
+    sprintf(p, "%d", num);
+    while(*++p);
+    return;
+  }
+  else 
+  {
+    int par = gen_rand_num(0,4);
+    if (!par) *p++ = '(';
+    dfs(dep-1);
+    if (!par) *p++ = ')'; // left
+    int op = gen_rand_num(0,3);
+    switch (op)
+    {
+    case 0:
+      *p++ = '+';
+      break;
+    
+    case 1:
+      *p++ = '-';
+      break;
+
+    case 2:
+      *p++ = '*';
+      break;
+
+    case 3:
+      *p++ = '/';
+      break;
+
+    default:
+      break;
+    }
+
+    par = gen_rand_num(0,4);
+    if (!par) *p++ = '(';
+    dfs(dep-1);
+    if (!par) *p++ = ')'; // right
+    return;
+  }
+}
+
 static inline void gen_rand_expr() {
-  buf[0] = '\0';
+  memset(buf, 0, sizeof(buf));
+  p = buf;
+  int dep = gen_rand_num(1, 9);
+  dfs(dep); // dfs parser tree 
 }
 
 static char code_buf[65536];
@@ -38,16 +95,20 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc .code.c -o .expr");
-    if (ret != 0) continue;
+    int ret = system("gcc .code.c -Werror -o .expr");
+    if (ret != 0)
+    {
+      i--;
+      continue;
+    }
 
     fp = popen("./.expr", "r");
     assert(fp != NULL);
 
     int result;
-    fscanf(fp, "%d", &result);
+    if(fscanf(fp, "%d", &result) != 1)
+      assert(0);
     pclose(fp);
-
     printf("%u %s\n", result, buf);
   }
   return 0;
