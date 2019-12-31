cf9bb06bc1538e4e842b705b36902459ec983fd8
tracer-ics2018 <tracer@njuics.org>
2019-12-31 09:52:52 +0800

    >  compile U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  09:52:52 up 39 days, 19:21,  3 users,  load average: 0.20, 0.18, 0.12 efa24ffbb1779bbb5440ccd8b7c1dca9f1247f44

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -27,23 +27,26 @@ int main(int argc, char *argv[]) {
     printf("cannot open file input\n");
     exit(1);
   }
-  unsigned res;
-  char str[2048];
-  bool success;
-  bool pass = true;
-  int cnt = 0;
-  while (fscanf(fp, "%u %[^\n]", &res, str) == 2)
-  {
-    printf("%u\t%s\n", res, str);
-    unsigned tmp = expr(str, &success);
-    printf("%d\n", tmp);
-    pass = pass && success && (tmp == res);
-    cnt++;
-  }
-  fclose(fp);
 
-  if (pass)
-    printf("success fully pass all %d test cases\n", cnt);
+  char str[] = "1+1";
+  bool success;
+  printf("%s = %d\n", str, expr(str, &success));
+  // unsigned res;
+  // char str[2048];
+  // bool pass = true;
+  // int cnt = 0;
+  // while (fscanf(fp, "%u %[^\n]", &res, str) == 2)
+  // {
+  //   printf("%u\t%s\n", res, str);
+  //   unsigned tmp = expr(str, &success);
+  //   printf("%d\n", tmp);
+  //   pass = pass && success && (tmp == res);
+  //   cnt++;
+  // }
+  // fclose(fp);
+
+  // if (pass)
+  //   printf("success fully pass all %d test cases\n", cnt);
   
 
   /* Initialize the monitor. */
