c6c4a4b53703358aeee65f3bac60e415da565cfb
tracer-ics2018 <tracer@njuics.org>
2019-12-31 15:26:01 +0800

    >  compile U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:26:01 up 40 days, 54 min,  3 users,  load average: 0.15, 0.10, 0.09 82aecc1825a5cd6011da07ce4341d521e917fa8e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -45,8 +45,6 @@ static struct rule
     {"0[xX][0-9a-fA-F]+", TK_HEX},    // hexadecimal
     {"[0-9][0-9]*", TK_DEC},          // decimal
     {"\\$[a-zA-Z][a-zA-Z]+", TK_REG}, // register
-    {"==", TK_EQ},                    // equal
-    {"!=", TK_NEQ},                   // not equal
     {"&&", TK_AND},                   // and
     {"\\|\\|", TK_OR},                // or
     {"!=", TK_NEQ},                   // not equal
