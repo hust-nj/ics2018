6515f187071406f5e2a4476b05a5d1e800a29442
tracer-ics2018 <tracer@njuics.org>
2019-12-31 19:16:19 +0800

    >  compile U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:16:19 up 40 days,  4:44,  3 users,  load average: 0.04, 0.08, 0.08 e67d20aeb16622d491178e85fa9c58524ede375a

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -5,50 +5,50 @@ void ui_mainloop(int);
 #include <stdio.h>
 #include <limits.h>
 
-#include "nemu.h"
-extern uint32_t expr(char *e, bool *success);
-extern void init_regex();
+// #include "nemu.h"
+// extern uint32_t expr(char *e, bool *success);
+// extern void init_regex();
 
 int main(int argc, char *argv[]) {
   
   /* test expr */
-   char cwd[1000];
-   if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       printf("Current working dir: %s\n", cwd);
-   } else {
-       perror("getcwd() error");
-       return 1;
-   }
+  //  char cwd[1000];
+  //  if (getcwd(cwd, sizeof(cwd)) != NULL) {
+  //      printf("Current working dir: %s\n", cwd);
+  //  } else {
+  //      perror("getcwd() error");
+  //      return 1;
+  //  }
 
-  init_regex();
-  FILE *fp;
-  if(!(fp = fopen("tools/gen-expr/input", "r")))
-  {
+  // init_regex();
+  // FILE *fp;
+  // if(!(fp = fopen("tools/gen-expr/input", "r")))
+  // {
     
-    printf("cannot open file input\n");
-    exit(1);
-  }
+  //   printf("cannot open file input\n");
+  //   exit(1);
+  // }
   
-  unsigned res;
-  char str[2048];
-  bool success;
-  bool pass = true;
-  int cnt = 0;
-  while (fscanf(fp, "%u %[^\n]", &res, str) == 2)
-  {
-    // printf("%u\t%s\n", res, str);
-    unsigned tmp = expr(str, &success);
-    // printf("%d\n", tmp);
-    pass = pass && success && (tmp == res);
-    if (!success) printf("fail at %d\n", cnt);
-    cnt++;
-  }
-  fclose(fp);
+  // unsigned res;
+  // char str[2048];
+  // bool success;
+  // bool pass = true;
+  // int cnt = 0;
+  // while (fscanf(fp, "%u %[^\n]", &res, str) == 2)
+  // {
+  //   // printf("%u\t%s\n", res, str);
+  //   unsigned tmp = expr(str, &success);
+  //   // printf("%d\n", tmp);
+  //   pass = pass && success && (tmp == res);
+  //   if (!success) printf("fail at %d\n", cnt);
+  //   cnt++;
+  // }
+  // fclose(fp);
 
-  if (pass)
-    printf("success fully pass all %d test cases\n", cnt);
-  else 
-    printf("error!\n");
+  // if (pass)
+  //   printf("success fully pass all %d test cases\n", cnt);
+  // else 
+  //   printf("error!\n");
 
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,23 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_wp()
+{
+  WP *node = free_->next; // get a node to use
+  if(node == NULL)
+    assert(0);
+  node->next = head->next; 
+  head = node; // add the node to busy list
+  free_->next = node->next; // delete the node from free_ list
+  return node;
+}
+
+void free_wp(WP *wp)
+{
+  
+}
+
+
+void free_wp(WP *wp);
+
 
