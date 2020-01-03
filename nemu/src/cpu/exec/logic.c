71ee517113d52e0d974b7b82d7c6212e053678ac
tracer-ics2018 <tracer@njuics.org>
2020-01-03 20:29:59 +0800

    >  compile U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:29:59 up 43 days,  5:58,  3 users,  load average: 0.13, 0.16, 0.15 35aec588505f1d8728d48a7654c6ff166225e717

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -2,25 +2,46 @@
 #include "cpu/cc.h"
 
 make_EHelper(test) {
-  TODO();
+	rtl_and(&t1, &id_dest->val, &id_src->val);
+	t0 = 0;
+	rtl_set_OF(&t0);
+	rtl_set_CF(&t0);
+	rtl_update_ZFSF(&t1, id_dest->width);
 
   print_asm_template2(test);
 }
 
 make_EHelper(and) {
-  TODO();
+	rtl_and(&t1, &id_dest->val, &id_src->val);
+	t0 = 0;
+
+	rtl_set_OF(&t0);
+	rtl_set_CF(&t0);
+	rtl_update_ZFSF(&t1, id_dest->width);
+	operand_write(id_dest, &t1);
 
   print_asm_template2(and);
 }
 
 make_EHelper(xor) {
-  TODO();
+	rtl_xor(&t1, &id_dest->val, &id_src->val);
+	t0 = 0;
+
+	rtl_set_OF(&t0);
+	rtl_set_CF(&t0);
+	rtl_update_ZFSF(&t1, id_dest->width);
+	operand_write(id_dest, &t1);
 
   print_asm_template2(xor);
 }
 
 make_EHelper(or) {
-  TODO();
+	rtl_or(&t2, &id_dest->val, &id_src->val);
+	operand_write(id_dest, &t2);
+	rtl_update_ZFSF(&t2, id_dest->width);
+	t0 = 0;
+	rtl_set_OF(&t0);
+	rtl_set_CF(&t0);
 
   print_asm_template2(or);
 }
