73f5dac947fcb01605bcb155446cb3a606ba3651
tracer-ics2018 <tracer@njuics.org>
2020-01-04 20:30:40 +0800

    >  compile U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:30:40 up 44 days,  5:58,  4 users,  load average: 0.10, 0.10, 0.09 20398bdb4d32becbb179431c437ac35e1f8e8ac1

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -29,7 +29,7 @@ make_EHelper(sub) {
 make_EHelper(cmp) {
   rtl_sub(&t0, &id_dest->val, &id_src->val);
   t1 = (t0 & (0xffffffffu >> ((4-id_dest->width) << 3)));
-  t3 = (t1 != t0);
+  t3 = t1 > id_dest->val;
 
 	rtl_set_CF(&t3);
 	rtl_update_ZFSF(&t1, id_dest->width);
