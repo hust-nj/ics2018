d1c5a1e270031fbd099235fd6617365d0ff66fc6
tracer-ics2018 <tracer@njuics.org>
2020-01-05 12:51:24 +0800

    >  run U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  12:51:24 up 44 days, 22:19,  4 users,  load average: 0.06, 0.09, 0.08 7ffe93272c77c182c61cde772c8ee3fc2facf2b9

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -1,13 +1,17 @@
 #include <am.h>
 #include <x86.h>
 #include <amdev.h>
+#define I8042_DATA_PORT 0x60
 
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      unsigned long long press = inl(I8042_DATA_PORT);
+      kbd->keycode = press;
+      if(press != _KEY_NONE){
+          kbd->keydown = !(kbd->keydown);
+      }
       return sizeof(_KbdReg);
     }
   }
