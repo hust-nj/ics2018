336e140191a210e27932b35dbd5b80a0410bd21f
tracer-ics2018 <tracer@njuics.org>
2020-01-05 21:51:59 +0800

    >  compile U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:51:59 up 45 days,  7:20,  4 users,  load average: 0.16, 0.09, 0.04 da31452dab19226f6c22bfafe2e8eb692b2acb25

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -44,7 +44,12 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  TODO();
+	rtl_pop(&decoding.jmp_eip);
+	rtl_j(decoding.jmp_eip);
+
+	rtl_pop(&t0);
+	cpu.cs = t0 & 0xffff;
+	rtl_pop(&cpu.flags);
 
   print_asm("iret");
 }
