a7fff9265d39a77995340ee2b38a2ba316b6d117
tracer-ics2018 <tracer@njuics.org>
2020-01-05 12:09:05 +0800

    >  run U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  12:09:05 up 44 days, 21:37,  4 users,  load average: 0.11, 0.07, 0.05 b5bf2a10312b64eac3506556ce5404596e3d2bb8

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -1,13 +1,16 @@
 #include <am.h>
 #include <x86.h>
 #include <amdev.h>
+#define RTC_PORT 0x48
+
 
 size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _UptimeReg *uptime = (_UptimeReg *)buf;
-      uptime->hi = 0;
-      uptime->lo = 0;
+      unsigned long long curtime = inl(RTC_PORT);
+      uptime->hi = curtime >> 32;
+      uptime->lo = curtime & 0xffffffff;
       return sizeof(_UptimeReg);
     }
     case _DEVREG_TIMER_DATE: {
