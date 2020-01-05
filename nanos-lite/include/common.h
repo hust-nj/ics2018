be26296bdba14a1309b1b844d2113d4f0bbf7e78
tracer-ics2018 <tracer@njuics.org>
2020-01-05 19:56:17 +0800

    >  compile U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:56:17 up 45 days,  5:24,  4 users,  load average: 0.12, 0.11, 0.06 edc0c1da46b23fecd21317afb11158777f9ce0d

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -2,7 +2,7 @@
 #define __COMMON_H__
 
 /* Uncomment these macros to enable corresponding functionality. */
-//#define HAS_CTE
+#define HAS_CTE
 //#define HAS_VME
 
 #include <am.h>
@@ -10,6 +10,8 @@
 #include "debug.h"
 
 typedef char bool;
+
+#define printk printf
 #define true 1
 #define false 0
 
