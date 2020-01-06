9ff4c25f2a8e43df4950c624af672edeaea3416a
tracer-ics2018 <tracer@njuics.org>
2020-01-06 14:55:39 +0800

    >  run U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:55:39 up 46 days, 23 min,  4 users,  load average: 0.05, 0.10, 0.09 cd4246f06d2aed70d96ed54a66593c269150a36

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,7 +2,12 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+	uintptr_t i = 0;
+	for(; len > 0; len--) {
+		_putc(((char*)buf)[i]);
+		i++;;
+	}
+	return i;
 }
 
 #define NAME(key) \
