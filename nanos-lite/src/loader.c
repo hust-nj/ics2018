4c17c691b9709814f7a8b6c8d77ca5a3adadd5ad
tracer-ics2018 <tracer@njuics.org>
2020-01-07 01:27:07 +0800

    >  run U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  01:27:07 up 46 days, 10:55,  4 users,  load average: 0.01, 0.06, 0.08 ca5f303830ec0cd44d464301630dd67e653422f3

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,19 +2,22 @@
 
 #define DEFAULT_ENTRY 0x4000000
 
-// 从ramdisk中`offset`偏移处的`len`字节读入到`buf`中
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern size_t fs_read(int fd, void *buf, size_t len);
+extern size_t fs_write(int fd, const void *buf, size_t len);
+extern size_t fs_lseek(int fd, size_t offset, int whence);
+extern int fs_close(int fd);
+extern size_t fs_filesz(int fd);
+extern size_t get_ramdisk_size();
 
-// 把`buf`中的`len`字节写入到ramdisk中`offset`偏移处
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-
-// 返回ramdisk的大小, 单位为字节
-size_t get_ramdisk_size();
 
 
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  ramdisk_read((void *)DEFAULT_ENTRY, 0, get_ramdisk_size());
+  // ramdisk_read((void *)DEFAULT_ENTRY, 0, get_ramdisk_size());
+  int fd = fs_open(filename, 0, 0);
+	fs_read(fd, (void *)DEFAULT_ENTRY, fs_filesz(fd));
+	fs_close(fd);
   return DEFAULT_ENTRY;
 }
 
