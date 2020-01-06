1c2f1cd9974637b57410b4bd51335ebc939fa4e7
tracer-ics2018 <tracer@njuics.org>
2020-01-07 02:54:11 +0800

    >  run U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  02:54:11 up 46 days, 12:22,  4 users,  load average: 0.12, 0.09, 0.09 54fe04710bedb5ee9bf3ba83b5e3f6535bff022

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -8,29 +8,28 @@ extern size_t events_read(void *buf, size_t offset, size_t len);
 extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
 extern size_t fb_write(const void *buf, size_t offset, size_t len);
 
-
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
 typedef struct {
-  char *name;
-  size_t size;
-  size_t disk_offset;
-  size_t open_offset;
-  ReadFn read;
-  WriteFn write;
+	char *name;
+	size_t size;
+	size_t disk_offset;
+	size_t open_offset;
+	ReadFn read;
+	WriteFn write;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_TTY};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
-  panic("should not reach here");
-  return 0;
+	panic("should not reach here");
+	return 0;
 }
 
 size_t invalid_write(const void *buf, size_t offset, size_t len) {
-  panic("should not reach here");
-  return 0;
+	panic("should not reach here");
+	return 0;
 }
 
 /* This is the information about all files in disk. */
@@ -42,7 +41,6 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_EVENTS] = {"/dev/events", 0, 0, 0, events_read},
   [FD_DISPINFO] = {"/proc/dispinfo", 128, 0, 0, dispinfo_read},
   [FD_TTY] = {"/dev/tty", 0, 0, 0, invalid_read, serial_write},
-
 #include "files.h"
 };
 
@@ -70,6 +68,18 @@ size_t fs_read(int fd, void *buf, size_t len) {
 		case FD_STDIN:
 		case FD_STDOUT:
 		case FD_STDERR:
+		case FD_FB:
+			break;
+		case FD_EVENTS:
+			len = file_table[fd].read(buf, 0, len);
+			break;
+		case FD_DISPINFO:
+			if (file_table[fd].open_offset >= file_table[fd].size)
+				return 0;
+			if (file_table[fd].open_offset + len > fs_size)
+				len = file_table[fd].size - file_table[fd].open_offset;
+			file_table[fd].read(buf, file_table[fd].open_offset, len);
+			file_table[fd].open_offset += len;
 			break;
 		default:
 			if(file_table[fd].open_offset >= fs_size)
@@ -87,9 +97,21 @@ size_t fs_write(int fd, const void *buf, size_t len) {
 	size_t fs_size = fs_filesz(fd);
 	switch(fd) {
 		case FD_STDIN: break;
+		case FD_TTY:
 		case FD_STDOUT:
 		case FD_STDERR:
 			file_table[fd].write(buf, 0, len);
+			break;
+		case FD_FB:
+			if(file_table[fd].open_offset >= fs_size)
+				return 0;
+			if(file_table[fd].open_offset + len > fs_size)
+				len = fs_size - file_table[fd].open_offset;
+			file_table[fd].write(buf, file_table[fd].open_offset, len);
+			file_table[fd].open_offset += len;
+			break;
+		case FD_EVENTS:
+		case FD_DISPINFO:
 			break;
 		default:
 			if(file_table[fd].open_offset >= fs_size)
@@ -140,4 +162,3 @@ size_t fs_filesz(int fd)
 {
 	return file_table[fd].size;
 }
-
