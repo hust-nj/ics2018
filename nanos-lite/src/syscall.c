8ba37ca237c2f0f68dccc2aa8ec884b0f469c576
tracer-ics2018 <tracer@njuics.org>
2020-01-06 10:42:09 +0800

    >  run U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:42:09 up 45 days, 20:10,  4 users,  load average: 0.10, 0.08, 0.08 e724284b515ef607c36419d8714e60e9ecb73d37

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,11 +3,18 @@
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
+  uintptr_t ret = -1;
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case SYS_yield:
+      ret = 0;
+      printf("system yeild\n");
+      break;
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
+  c->GPR1 = ret;
   return NULL;
 }
