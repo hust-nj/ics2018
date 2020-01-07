77642edb3e1e6693460d469b7a8d9ebcbac68104
tracer-ics2018 <tracer@njuics.org>
2020-01-07 08:18:14 +0800

    >  run U201614787 v-jining Linux MSRAVCG06 4.4.0-169-generic #198-Ubuntu SMP Tue Nov 12 10:38:00 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:18:14 up 46 days, 17:46,  4 users,  load average: 0.66, 0.48, 0.28 11e804d5738e0319cbf28f2de8195adf2e546d2

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -52,7 +52,7 @@ void init_fs() {
 
 int fs_open(const char *pathname, int flags, int mode) {
 	int i;
-  printf("pathname: %s\n", pathname);
+  // printf("pathname: %s\n", pathname);
 	for (i = 0; i < NR_FILES; i++) {
 		if (strcmp(file_table[i].name, pathname) == 0) {
 			file_table[i].open_offset = 0;
